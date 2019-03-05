8099a07c8e8e6a6530852fa5d9b4004c517cca5f
Nolan Ehrstrom <github@nolanpro.com>
2019-03-05 13:55:34 -0800

    Remove unused api_client

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -38,7 +38,6 @@ if(!file_exists(SCRIPT_PATH)) {
 if (getenv('API_TOKEN')) {
     $api_config = new OpenAPI\Client\Configuration();
     $api_config->setAccessToken(getenv('API_TOKEN'));
-    $api_client = new GuzzleHttp\Client();
 }
 
 $response = require(SCRIPT_PATH);
