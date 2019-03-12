1b79a610b9fbf49bb2098d6658b3acccaeca6ab8
Nolan Ehrstrom <github@nolanpro.com>
2019-03-11 19:10:57 -0700

    Add dockblocks to the api class

--- src/Api.php
+++ src/Api.php
@@ -1,18 +1,56 @@
 <?php
 namespace Executor;
 
+/**
+ * This helper allows you to use Api classes in the namespace
+ * ProcessMaker\Client\Api\ by calling its name as a function
+ * on an instance of this class. The class name is lowercase first.
+ * 
+ * For example, to call \ProcessMaker\Client\Api\ProcessesApi
+ * call $api->processes()
+ * 
+ */
 class Api {
+
+    /**
+     * Reusable instances of api objects
+     *
+     * @var array
+     */
     private $instances = [];
 
+    /**
+     * Instance of the Guzzle client
+     *
+     * @var \GuzzleHttp\Client
+     */
     private $client;
 
+    /**
+     * Instance of the client configuration object
+     *
+     * @var \ProcessMaker\Client\Configuration
+     */
     private $config;
 
+    /**
+     * Constructor
+     *
+     * @param \ProcessMaker\Client\Configuration $config
+     * @param boolean $ssl_verify
+     */
     public function __construct($config, $ssl_verify = true) {
         $this->client = new \GuzzleHttp\Client(['verify' => $ssl_verify]);
         $this->config = $config;
     }
 
+    /**
+     * Method to catch all undefined methods
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return object
+     */
     public function __call($name, $arguments)
     {
         if (count($arguments) > 0) {
@@ -33,6 +71,12 @@ class Api {
         }
     }
 
+    /**
+     * Translate the called name to a real class
+     *
+     * @param string $name
+     * @return string
+     */
     private function getClassName($name)
     {
         return '\\ProcessMaker\\Client\Api\\' . ucfirst($name) . "Api";
