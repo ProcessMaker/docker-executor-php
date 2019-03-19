5ab60869b820df5f692327a4ad2b8e4cfb1923a0
Nolan Ehrstrom <github@nolanpro.com>
2019-03-19 16:45:18 -0700

    Clone and copy the SDK so it will be available for docker build

--- build.sh
+++ build.sh
@@ -3,14 +3,19 @@ set -x
 
 BRANCH=${BRANCH:=master}
 TAG=${TAG:=dev-$BRANCH}
-REPO=${REPO:=ProcessMaker/pm4-sdk-php}
 EXECUTOR_IMAGE="processmaker/pm4-docker-executor-php:${TAG}"
 
 pushd src
+  if [[ ! -d "php-sdk" ]]; then
+    git clone --branch $BRANCH --depth 1 https://github.com/ProcessMaker/pm4-sdk-php.git php-sdk
+  fi
+  rm -rf composer.lock
+  rm -rf vendor
   composer install
   ./run_tests.sh
 popd
 
 docker build -t $EXECUTOR_IMAGE .
+rm -rf php-sdk
 
 # docker push $EXECUTOR_IMAGE
