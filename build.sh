2a65faaf1930a6fbb631a96f742a852c99ef5983
Nolan Ehrstrom <github@nolanpro.com>
2019-05-06 17:54:03 -0400

    Fix names in build script

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ set -x
 
 BRANCH=${BRANCH:=master}
 TAG=${TAG:=dev-${BRANCH//[\/]/-}}
-EXECUTOR_IMAGE="processmaker/-docker-executor-php:${TAG}"
+EXECUTOR_IMAGE="processmaker/spark-docker-executor-php:${TAG}"
 
 pushd src
-  if [[ ! -d "-sdk-php" ]]; then
-    git clone --branch $BRANCH --depth 1 https://github.com/ProcessMaker/-sdk-php.git
+  if [[ ! -d "spark-sdk-php" ]]; then
+    git clone --branch $BRANCH --depth 1 https://github.com/ProcessMaker/spark-sdk-php.git
   fi
   rm -rf composer.lock
   rm -rf vendor
@@ -16,6 +16,4 @@ pushd src
 popd
 
 docker build -t $EXECUTOR_IMAGE .
-rm -rf src/-sdk-php
-
-# docker push $EXECUTOR_IMAGE
+rm -rf src/spark-sdk-php
